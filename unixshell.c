cc2dfa1469d4970c8b534947ff86a25654ddf5cb
Karan Vishwamitran <karan.vishwamitran@outlook.com>
2025-08-07 06:54:34 -0700

    Implemented I/O redirection (<, >, >>)

--- unixshell.c
+++ unixshell.c
@@ -1,3 +1,4 @@
+#include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -10,6 +11,12 @@ char* read_input();
 char** split_input(char* in);
 bool execute(char** args);
 bool launch(char** args);
+void io_redirect(bool inflag, bool outflag, bool outflagappend, char* infile, char* outfile);
+char infile[256] = "";
+char outfile[256] = "";
+bool inflag;
+bool outflag;
+bool outflagappend;
 
 int main(int argc, char **argv)
 {
@@ -25,6 +32,11 @@ void loop()
 	bool status = true;
 	while(status)
 	{
+		inflag = false;
+		outflag = false;
+		outflagappend = false;
+		strcpy(infile, "");
+		strcpy(outfile, "");
 		printf("> ");
 		in = read_input();
 		args = split_input(in);
@@ -77,7 +89,85 @@ char** split_input(char* in)
 		strcpy(arguments[j], args[j]);
 	}
 	arguments[i] = NULL;
+
+	for(int k = 0; k < i; k++)
+	{
+		if((strcmp(arguments[k], ">")) == 0 || (strcmp(arguments[k], ">>")) == 0)
+		{
+			if(arguments[k+1] == NULL)
+			{
+				perror("Invalid command");
+				exit(EXIT_FAILURE);
+			}
+			strcpy(outfile,arguments[k+1]);
+			outflag = true;
+			if(strcmp(arguments[k], ">>") == 0)
+			{
+				outflagappend = true;
+			}
+			free(arguments[k]);
+			free(arguments[k+1]);
+			for(int m = k; m < i - 2; m++)
+			{
+				arguments[m] = arguments[m + 2];
+			}
+			i -= 2;
+			k--;
+		}
+		else if(strcmp(arguments[k], "<") == 0)
+		{
+			if(arguments[k+1] == NULL)
+                        {
+                                perror("Invalid command");
+                                exit(EXIT_FAILURE);
+                        }
+			strcpy(infile, arguments[k+1]);
+			inflag = true;
+			free(arguments[k]);
+			free(arguments[k+1]);
+			for(int m = k; m < i - 2; m++)
+                        {
+                                arguments[m] = arguments[m + 2];
+                        }
+			i -= 2;
+			k--;
+		}
+	}
 	return arguments;
+}
+
+void io_redirect(bool inflag, bool outflag, bool outflagappend, char* infile, char* outfile)
+{
+	if(inflag)
+	{
+		int fd = open(infile, O_RDONLY);
+		if(fd == -1)
+		{
+			perror("open");
+			exit(EXIT_FAILURE);
+		}
+		dup2(fd, 0);
+		close(fd);
+	}
+	if(outflag)
+	{
+		int fd = -1;
+		if(outflagappend)
+		{
+			fd = open(outfile, O_WRONLY | O_CREAT | O_APPEND);
+		}
+		else
+		{
+			fd = open(outfile, O_WRONLY | O_CREAT | O_TRUNC);
+		}
+		if(fd == -1)
+		{
+			perror("open");
+			exit(EXIT_FAILURE);
+		}
+		dup2(fd, 1);
+		close(fd);
+	}
 }
 
 bool execute(char** args)
@@ -98,6 +188,10 @@ bool launch(char** args)
 	pid_t pid = fork();
 	if(pid == 0)
 	{
+		if(outflag || inflag)
+        	{
+                	io_redirect(inflag, outflag, outflagappend, infile, outfile);
+        	}
 		int status = execvp(args[0], args);
 		if(status == -1)
 		{
@@ -116,5 +210,7 @@ bool launch(char** args)
 	}
 	return true;
 }
+
+
 
 
